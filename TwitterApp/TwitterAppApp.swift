9dfc5c40c7cb0ff985b34a603926d3d20b64396b
Jai <jai07rajput@gmail.com>
2024-01-01 10:41:59 +0530

    first commit

---
+++ TwitterApp/TwitterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterAppApp.swift
+//  TwitterApp
+//
+//  Created by Jai  on 01/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TwitterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
