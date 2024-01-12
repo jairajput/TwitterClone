2608fbebd72907a0d067ee69894607cfc49ef840
Jai <jai07rajput@gmail.com>
2024-01-12 14:28:28 +0530

    Adding-Extension-headerView-ProfileView

--- TwitterApp/Core/Profile/Views/ProfileView.swift
+++ TwitterApp/Core/Profile/Views/ProfileView.swift
@@ -10,30 +10,7 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View {
         VStack{
-            ZStack(alignment: .bottomLeading)
-            {
-                Color(.systemBlue)
-                    .ignoresSafeArea()
-                
-                
-                VStack {
-                    
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "arrow.left")
-                            .resizable()
-                            .frame(width: 20 , height: 20)
-                            .foregroundColor(.white)
-                            .offset(x:16, y:12)
-                        
-                    }
-                    
-                    Circle()
-                        .frame(width: 72 , height: 72)
-                    .offset(x:16 , y:27)
-                }
-            }
+            headerView
             .frame(height: 97)
             Spacer()
         }
@@ -44,4 +21,33 @@ struct ProfileView: View {
     ProfileView()
 }
 
+
+extension ProfileView{
+    var headerView: some View {
+        ZStack(alignment: .bottomLeading)
+        {
+            Color(.systemBlue)
+                .ignoresSafeArea()
+            
+            
+            VStack {
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .resizable()
+                        .frame(width: 20 , height: 20)
+                        .foregroundColor(.white)
+                        .offset(x:16, y:12)
+                    
+                }
+                
+                Circle()
+                    .frame(width: 72 , height: 72)
+                .offset(x:16 , y:27)
+            }
+        }
+    }
+}
 
