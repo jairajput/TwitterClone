8ce9e0ac33a0d14acd9eed1525561e39ab3d7f0b
Jai <jai07rajput@gmail.com>
2024-01-21 14:08:37 +0530

    Info-PList

--- TwitterApp/Core/Authentication/View/LoginView.swift
+++ TwitterApp/Core/Authentication/View/LoginView.swift
@@ -11,66 +11,70 @@ struct LoginView: View {
     @State private var email = ""
     @State private var passsword = ""
     var body: some View {
-        VStack{
-            AuthHeaderView(title1: "Hello", title2: "Welcome Back")
-            
-            
-            VStack(spacing: 40){
-            CustomInputField(imageName: "envelope", placeHolderText: "Email", text: $email)
-                CustomInputField(imageName: "lock", placeHolderText: "Password", text: $passsword)
-        }
-            .padding(.horizontal,32)
-            .padding(.top, 44)
-            
-            
-            HStack{
-                Spacer()
-                NavigationLink{
-                    Text("Reset Password")
-                } label: {
-                    Text("Forgot Password")
-                        .font(.caption)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(.systemBlue))
-                        .padding(.top)
-                        .padding(.trailing , 24)
-                }
-            }
-            Button {
-                print("Sign In here")
-            } label: {
-                Text("Sign In")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(width: 340, height: 50)
-                    .background(Color(.systemBlue))
-                    .clipShape(Capsule())
-                    .padding()
-            }
-            .shadow(color: .gray.opacity(0.5), radius: 10 , x:0 , y:0)
-            
-            Spacer()
-            
-            NavigationLink{
-                RegistrationView()
-                    .navigationBarHidden(true)
-            }label: {
-                HStack {
-                    Text("Dont have an Account?")
-                        .font(.footnote)
-                    Text("Signup")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                }
+        
+        NavigationView {
+            VStack{
+                AuthHeaderView(title1: "Hello", title2: "Welcome Back")
                 
-                    
+                
+                VStack(spacing: 40){
+                CustomInputField(imageName: "envelope", placeHolderText: "Email", text: $email)
+                    CustomInputField(imageName: "lock", placeHolderText: "Password", text: $passsword)
             }
-            .foregroundColor(Color(.systemBlue))
-            .padding(.bottom , 32)
-            
-        }
-        .ignoresSafeArea()
+                .padding(.horizontal,32)
+                .padding(.top, 44)
+                
+                
+                HStack{
+                    Spacer()
+                    NavigationLink{
+                        Text("Reset Password")
+                    } label: {
+                        Text("Forgot Password")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(.systemBlue))
+                            .padding(.top)
+                            .padding(.trailing , 24)
+                    }
+                }
+                Button {
+                    print("Sign In here")
+                } label: {
+                    Text("Sign In")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 340, height: 50)
+                        .background(Color(.systemBlue))
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                .shadow(color: .gray.opacity(0.5), radius: 10 , x:0 , y:0)
+                
+                Spacer()
+                
+                NavigationLink{
+                    RegistrationView()
+                        .navigationBarHidden(true)
+                    
+                } label: {
+                    HStack{
+                        Text("Dont Have An Account?")
+                            .font(.footnote)
+                        
+                        Text("Sign Up")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                    }
+                }
+                .padding(.bottom, 32)
+                .foregroundColor(Color(.systemBlue))
+            }
+            .ignoresSafeArea()
         .navigationBarHidden(true)
+        }
+
     }
 }
 
--- TwitterApp/TwitterAppApp.swift
+++ TwitterApp/TwitterAppApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TwitterAppApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
 //         ContentView()
